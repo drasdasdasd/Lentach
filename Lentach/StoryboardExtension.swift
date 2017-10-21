738696c3858a9aa35f845f91fa41131d8060c278
Dzianis Baidan <d.baydan@blak-it.com>
2017-10-21 16:10:10 +0300

    [+] Change carousel

---
+++ Lentach/StoryboardExtension.swift
@@ -0,0 +1,17 @@
+//
+//  StoryboardExtension.swift
+//  Fjuul
+//
+//  Created by Dzianis Baidan on 23.05.17.
+//  Copyright © 2017 Fjuul Vision Oy. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    
+    convenience init(storyboard: Storyboard, bundle: Bundle? = nil) {
+        self.init(name: storyboard.filename, bundle: bundle)
+    }
+    
+}
