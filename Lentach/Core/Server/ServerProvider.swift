a9c885c011a347c9cc25e4de6b11913204c232d5
Dzianis Baidan <d.baydan@blak-it.com>
2017-10-21 13:42:25 +0300

    [+] Initial commit

---
+++ Lentach/Core/Server/ServerProvider.swift
@@ -0,0 +1,70 @@
+//
+//  ServerProvider.swift
+//  Lentach
+//
+//  Created by Dzianis Baidan on 20.10.17.
+//  Copyright © 2017 Dzianis Baidan. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum ServerService {
+    
+    case listOfTask
+    
+}
+
+// MARK: -
+// MARK: - Moya extension
+
+extension ServerService: TargetType {
+    
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+    var baseURL: URL {
+        return URL(string: "http://92.53.102.42:3000")!
+    }
+    
+    var path: String {
+        switch self {
+        case .listOfTask:
+            return "/api/tasks"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .listOfTask:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        switch self {
+        default:
+            return "{}".data(using: String.Encoding.utf8)!
+        }
+    }
+    
+    var parameters: [String: Any]? {
+        switch self {
+        case .listOfTask:
+            return nil
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .listOfTask:
+            return URLEncoding.default
+        }
+    }
+    
+}
---
+++ Lentach/Group/Feed/Controller/FeedController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lentach
+//
+//  Created by Dzianis Baidan on 20.10.17.
+//  Copyright © 2017 Dzianis Baidan. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+        
+}
+
